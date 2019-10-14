5041da37fdbacd3338c4feeb6fb077b6cbf73262
Carlos Huchim Ahumada <huchim@live.com.mx>
2019-10-14 16:34:26 -0500

    cambios

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
 <?php
+
 include "vendor/autoload.php";
 
-if (file_exists("config.php")) {
-    include "config.php";
+$pipelinesConfig = glob('config/*.php');
+
+foreach ($pipelinesConfig as $pipelineFile) {
+    if (file_exists($pipelineFile)) {
+        include $pipelineFile;
+    }
 }
 
 $whitelist = array('127.0.0.1', '::1');
@@ -14,9 +19,9 @@ $app = new \Slim\App([
         "displayErrorDetails" => APP_DEBUG,
         "determineRouteBeforeAppMiddleware" => true
     ]
-]);
+        ]);
 
-$app->add(function ($request, $response, $next) {    
+$app->add(function ($request, $response, $next) {
     if (!APP_DEBUG) {
         if ($request->getUri()->getScheme() !== "https") {
             // No acceder a la aplicación de manera simple.
@@ -25,7 +30,7 @@ $app->add(function ($request, $response, $next) {
     }
 
     $response = $next($request, $response);
-    
+
     return $response;
 });
 
@@ -33,8 +38,8 @@ $app->get("/", Huchim\Controller::getAction("help"));
 $app->get("/{host}/{stage}/lock/{timestamp}", Huchim\Controller::getAction("lock"));
 $app->get("/{host}/{stage}/timestamps", Huchim\Controller::getAction("timestamps"));
 $app->get("/{host}/{stage}/unlock/{timestamp}", Huchim\Controller::getAction("unlock"));
-$app->get("/{host}/{stage}/snapshot/{lock}[/{timestamp}]",  Huchim\Controller::getAction("snapshot"));
-$app->get("/{host}/{stage}/diff/{timestamp}/{to}",  Huchim\Controller::getAction("diff"));
+$app->get("/{host}/{stage}/snapshot/{lock}[/{timestamp}]", Huchim\Controller::getAction("snapshot"));
+$app->get("/{host}/{stage}/diff/{timestamp}/{to}", Huchim\Controller::getAction("diff"));
 $app->post("/{host}/{stage}/upload/{timestamp}", Huchim\Controller::getAction("upload"));
 $app->post("/{host}/{stage}/deploy/{timestamp}", Huchim\Controller::getAction("deploy"));
 $app->post("/{host}/{stage}/diff/{timestamp}", Huchim\Controller::getAction("diff_post"));
@@ -58,12 +63,12 @@ $app->get("/{host}/{stage}/lock/{timestamp}", function($request, $response, $arg
     if ($statusCode !== 200) {
         return $response->withStatus($statusCode);
     }
-    
+
     $c = file_put_contents("timestamps/{$hostKey}_{$stageName}.lock", $timestamp);
-    
+
     return $response
             ->withHeader("Location", "/{$hostKey}/{$stageName}/unlock/{$timestamp}")
-            ->withStatus(201);    
+            ->withStatus(201);
 });
 
 $app->post("/{host}/{stage}/upload/{timestamp}", function(Request $request, Response $response, $args) use ($hosts) {
@@ -76,22 +81,22 @@ $app->post("/{host}/{stage}/upload/{timestamp}", function(Request $request, Resp
     if ($statusCode !== 200) {
         return $response->withStatus($statusCode);
     }
-    
+
     $uploadedFiles = $request->getUploadedFiles();
-    
+
     if (count($uploadedFiles) === 0) {
         return $response->withStatus(400);
     }
-    
+
     // "file" es el nombre, pero no debería asumirlo.
     $fileInputKey = array_keys($uploadedFiles)[0];
     $firstFileUpload = $uploadedFiles[$fileInputKey];
-    $path = "timestamps/{$hostKey}_{$stageName}_{$timestamp}.zip";    
-    
+    $path = "timestamps/{$hostKey}_{$stageName}_{$timestamp}.zip";
+
     // Subir.
     $firstFileUpload->moveTo($path);
-    
-    return $response->withStatus(201);    
+
+    return $response->withStatus(201);
 });
 
 $app->get("/{host}/{stage}/unlock/{timestamp}", function($request, $response, $args) use ($hosts) {
@@ -105,11 +110,11 @@ $app->get("/{host}/{stage}/unlock/{timestamp}", function($request, $response, $a
     if ($statusCode !== 200) {
         return $response->withStatus($statusCode);
     }
-    
+
     $c = unlink("timestamps/{$hostKey}_{$stageName}.lock");
-    
+
     return $response
-            ->withStatus(201);    
+            ->withStatus(201);
 });
 
 // Crea un listado de archivos actuales y los guarda con el nuevo nombre.
@@ -126,47 +131,47 @@ $app->get("/{host}/{stage}/create[/{timestamp}]", function($request, $response, 
     if ($statusCode !== 200) {
         return $response->withStatus($statusCode);
     }
-    
+
     if (file_exists("timestamps/{$hostKey}_{$stageName}.lock")) {
         $currentTimestamp = file_get_contents("timestamps/{$hostKey}_{$stageName}.lock");
-        
-        if ($currentTimestamp !== $timestamp) {        
+
+        if ($currentTimestamp !== $timestamp) {
             return $response->withStatus(503);
         }
     }
-    
+
     $host = $hosts[$hostKey];
     $stage = $host["stages"][$stageName];
     $excludes = isset($host["exclude"]) ? $host["exclude"] : [];
     $users = isset($stage["users"]) ? $stage["users"] : [];
-    
+
     if (count($users) > 0) {
         // Require Login
         $usr = $request->getServerParam("PHP_AUTH_USER", "");
         $pwd = $request->getServerParam("PHP_AUTH_PW", "");
-        
+
         if (!in_array($usr, $users)) {
             return $response->withStatus(401);
         }
     }
-    
+
     if (!isset($stage["deploy_path"])) {
         return $response->withStatus(500);
     }
-    
+
     $deploy_path = $stage["deploy_path"];
-    
+
     // Lista de archivos.
     $output = [
         "commit" => $commit,
         "files" => listFolderFiles($deploy_path, $excludes, $deploy_path)
     ];
-    
+
     $outputString = \json_encode($output);
     $c = file_put_contents("timestamps/{$hostKey}_{$stageName}_{$timestamp}_changes.json", $outputString);
 
     return $response->withStatus(201);
-    
+
 });
 
 // Muestra los cambios entre el timestamp y el actual.
@@ -187,33 +192,33 @@ $app->get("/{host}/{stage}/diff/{timestamp}", function($request, $response, $arg
     $stage = $host["stages"][$stageName];
     $excludes = isset($host["exclude"]) ? $host["exclude"] : [];
     $users = isset($stage["users"]) ? $stage["users"] : [];
-    
+
     if (count($users) > 0) {
         // Require Login
         $usr = $request->getServerParam("PHP_AUTH_USER", "");
         $pwd = $request->getServerParam("PHP_AUTH_PW", "");
-        
+
         if (!in_array($usr, $users)) {
             return $response->withStatus(401);
         }
     }
-    
+
     if (!isset($stage["deploy_path"])) {
         return $response->withStatus(500);
     }
-    
+
     $deploy_path = $stage["deploy_path"];
-    
+
     // Elegir el archivo
     if (!file_exists("timestamps/{$hostname}_{$stageName}_{$timestamp}_changes.json")) {
         $sourceRawText = \json_encode(["files" => []]);
     } else {
         $sourceRawText = file_get_contents("timestamps/{$hostname}_{$stageName}_{$timestamp}_changes.json");
     }
-    
+
     $sourceFileList = \json_decode($sourceRawText, true);
-    
-    
+
+
     $currentFileList = listFolderFiles($deploy_path, $excludes, $deploy_path);
 
     $m = implode("\n", compareFileList($sourceFileList["files"], $currentFileList));
@@ -222,8 +227,8 @@ $app->get("/{host}/{stage}/diff/{timestamp}", function($request, $response, $arg
             ->withHeader("Content-Type", "text/plain")
             ->getBody()
             ->write($m);
-    
-    return $response;    
+
+    return $response;
 });
 
 $app->run();
@@ -236,26 +241,26 @@ function compareFileList($sourceList, $targetList) {
         if (array_key_exists($fileKey, $sourceList)) {
             continue;
         }
-        
+
         $diff[] = "+ " . $fileValue["name"];
     }
-    
+
     // Buscar todos los QUE NO EXISTEN en target.
     // Son los que han cambiado o la fecha o el nombre.
     foreach ($sourceList as $fileKey => $fileValue) {
         if (array_key_exists($fileKey, $targetList)) {
             continue;
         }
-        
+
         // Si el mismo archivo ya existe en el arreglo, se considera entonces
         // que fue modificado.
         $k = "+ " . $fileValue["name"];
-        
+
         if (!in_array($k, $diff)) {
             $diff[] = "- " . $fileValue["name"];
         }
     }
-    
+
     return $diff;
 }
 
@@ -275,13 +280,13 @@ function listFolderFiles($dir, $excludes = [], $baseDir = "", $separator = DIREC
         if (in_array($ff, $excludes)) {
             continue;
         }
-        
+
         $currentItem = $dir.$separator.$ff;
         $isDirectory = is_dir($currentItem);
 
         if ($isDirectory) {
             $directoryContent = listFolderFiles($currentItem, $excludes, $baseDir);
-            
+
             foreach ($directoryContent as $file) {
                 $files[] = $file;
             }
@@ -296,7 +301,7 @@ function listFolderFiles($dir, $excludes = [], $baseDir = "", $separator = DIREC
             ];
         }
     }
-    
+
     return $files;
 }
 
@@ -308,18 +313,18 @@ function getRequestCode($hostname, $stage, $client, $hosts) {
     if (!in_array($client, ["win", "linux"])) {
         return 400;
     }
-    
+
     if ($hostname === "" || $stage === "") {
         return 400;
     }
-    
+
     $host = $hosts[$hostname];
 
     if (!isset($host["stages"][$stage])) {
         return 404;
     }
-    
+
     return 200;
 }
- * 
+ *
  */
